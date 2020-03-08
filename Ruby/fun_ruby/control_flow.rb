94d6df6be71f7c193536eff9649e7cde8046ef7b
小俣圭佑 <omatakeisuke@MacBook.local>
2020-03-08 21:17:10 +0900

    add data_structure.rb

--- Ruby/fun_ruby/control_flow.rb
+++ Ruby/fun_ruby/control_flow.rb
@@ -3,3 +3,16 @@ if 条件　(then)
 begin
   命令
 end
+
+
+i = 1
+while 条件 (do)
+  命令
+  #rubyはi++不可
+  i += 1
+end
+
+
+繰り返し回数.times do
+  命令
+end
--- Ruby/fun_ruby/method.rb
+++ Ruby/fun_ruby/method.rb
@@ -1,7 +1,7 @@
 print("Hello, Ruby.\n")
 print "Hello, Ruby.\n"
 
-# 改行あり
+#改行あり
 puts "Hello, Ruby."
 p "Hello, Ruby."
 p 100
