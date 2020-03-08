8705a5f82cdfc59682ae2481fa8dc27a5cab85ea
小俣圭佑 <omatakeisuke@MacBook.local>
2020-03-08 21:19:27 +0900

    update method.rb

--- Ruby/fun_ruby/method.rb
+++ Ruby/fun_ruby/method.rb
@@ -1,6 +1,5 @@
 print("Hello, Ruby.\n")
 print "Hello, Ruby.\n"
-
 #改行あり
 puts "Hello, Ruby."
 p "Hello, Ruby."
@@ -10,3 +9,7 @@ p "100"
 #変数の展開
 a = 100
 p "数字は#{a}"
+
+#大きさの取得
+a = (1, 2, 3, 4)
+a.size => 4
