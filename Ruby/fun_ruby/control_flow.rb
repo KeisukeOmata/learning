a774ee3ec95d5ab2cd8760573b1bc56f782fe4d9
小俣圭佑 <omatakeisuke@MacBook.local>
2020-03-08 21:03:31 +0900

    add control_flow.rb

---
+++ Ruby/fun_ruby/control_flow.rb
@@ -0,0 +1,5 @@
+if 条件　(then)
+  命令
+begin
+  命令
+end
---
+++ Ruby/fun_ruby/method.rb
@@ -0,0 +1,12 @@
+print("Hello, Ruby.\n")
+print "Hello, Ruby.\n"
+
+# 改行あり
+puts "Hello, Ruby."
+p "Hello, Ruby."
+p 100
+p "100"
+
+#変数の展開
+a = 100
+p "数字は#{a}"
