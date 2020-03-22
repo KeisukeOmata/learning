7694fd819899d6195af3e5f9039d7b88fd2bad2d
小俣圭佑 <omatakeisuke@MacBook.local>
2020-03-22 19:02:28 +0900

    [update] memo.rb

--- Ruby/rails/memo.rb
+++ Ruby/rails/memo.rb
@@ -1,10 +1,27 @@
 rails s -b 0.0.0.0
-  rails new hoge
-  rails g model Hoge name:string
-  マイグレーションファイルにカラムを追加
-  rails db:migrate
-  rails g migration Addカラム名Toテーブル名 カラム名:データ型 カラム名:データ型
-  # $ rails g migration AddNameToPosts name:string
-  # https://ruby-rails.hatenadiary.com/entry/20140810/1407634200#migration-change-column-null
-  rails g controller blogs show index new edit
-  # $ rails g devise:controllers admins
+localhost:3000
+
+rails new hoge
+rails g controller hoges new edit show index 
+#rails g model Hoge name:stringでもよい 
+rails g model Hoge name:string
+マイグレーションファイルにカラムを追加
+rails db:migrate
+#カラムの追加と削除
+# rails g migration AddNameToPosts category:string body:text
+rails g migration Addカラム名Toテーブル名 カラム名:データ型 カラム名:データ型
+rails g migration Removeカラム名Fromテーブル名 カラム名:型名
+rails db:migrate
+#必殺技
+rails db:migrate:reset
+
+#refile
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
+sudo apt-get update
+sudo apt-get -y install imagemagick libmagick++-dev
+bundle install
+image_idカラムを追加
+rails db:migrate
+モデルに attachment :image を追加
+runtimeエラーが出た場合は config/initializers/application_controller_renderer.rb にシークレットキーを追加
