b04d373e1c4031f5e849069f9201f354c73f3fd8
小俣圭佑 <omatakeisuke@MacBook.local>
2020-04-13 18:31:23 +0900

    [update] main.c

--- Ruby/rails/other/c/hoge.c
+++ Ruby/rails/other/c/hoge.c
@@ -1,6 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 #include "hoge.h"
 
-int hoge(int num) {
-  num = 1;
-  return num;
+void hoge(int a, int b){
+  printf("a = %d b = %d\n", a, b);
+}
+
+// 関数はポインタ
+void fuga(int* a, int* b){
+  // 呼び出し元は＆で渡す
+  printf("a = %p b = %p\n", &a, &b);
+  // ポインタであれば戻り値以外だけでなく複数の値を変更可能
+  *a = 100;
+  *b = 200;
 }
--- Ruby/rails/other/c/main.c
+++ Ruby/rails/other/c/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include "hoge.h"
+ 
+int main(){
+    // 宣言は普通
+    int a = 0;
+    int b = 1;
+    char piyo[6 + 1];
 
-int main() {
-  // 変数宣言
-  int num = 0;
-  int i = 0;
-  char s[4 + 1];
-  // 初期化
-  memset(s, 0, sizeof(s));
-  // 値の設定
-  strcpy(s, "hoge");
+    memset(piyo, 0, sizeof(piyo));
 
-  num = hoge(num);
-  printf("Hell%d %s\n", num, s);
+    strcpy(piyo, "hogera");
+
+    printf("%s\n", piyo);
+
+    hoge(a, b);
+    // 呼び出し元は＆で渡す
+    fuga(&a, &b);
+    hoge(a, b);
 }
