4c5d532d09f16fe5bdaa4a6cdff3ee1d2f2b52a9
小俣圭佑 <omatakeisuke@MacBook.local>
2020-04-15 10:35:06 +0900

    [update] c

--- Ruby/rails/other/c/hoge.c
+++ Ruby/rails/other/c/hoge.c
@@ -2,15 +2,42 @@
 #include <string.h>
 #include "hoge.h"
 
-void hoge(int a, int b){
-  printf("a = %d b = %d\n", a, b);
+static int number(int, int);
+// プロトタイプ宣言はポインタ
+static int pointer(int*, int*);
+
+int hoge(){
+    // 宣言はポインタではない
+    int a = 0;
+    int b = 1;
+    int n = 0;
+    char piyo[6 + 1];
+
+    memset(piyo, 0, sizeof(piyo));
+
+    strcpy(piyo, "hogera");
+
+    printf("%s\n", piyo);
+
+    n = number(a, b);
+    // 呼び出し元は＆で渡す
+    n = pointer(&a, &b);
+    n = number(a, b);
+
+    return (0);
+}
+
+static int number(int a, int b){
+    printf("a = %d b = %d\n", a, b);
+    return (0);
 }
 
 // 関数はポインタ
-void fuga(int* a, int* b){
-  // 呼び出し元は＆で渡す
-  printf("a = %p b = %p\n", &a, &b);
-  // ポインタであれば戻り値以外だけでなく複数の値を変更可能
-  *a = 100;
-  *b = 200;
+static int pointer(int* a, int* b){
+    // 呼び出し元は＆で渡す
+    printf("a = %p b = %p\n", &a, &b);
+    // ポインタであれば戻り値以外だけでなく複数の値を変更可能
+    *a = 100;
+    *b = 200;
+    return (0);
 }
--- Ruby/rails/other/c/hoge.h
+++ Ruby/rails/other/c/hoge.h
@@ -1,8 +1,6 @@
 #ifndef _HOGE_H_
 #define _HOGE_H_
 
-void hoge(int, int);
-// プロトタイプ宣言はポインタ
-void fuga(int*, int*);
+int hoge();
 
 #endif // _HOGE_H_
