42586019fc9a8bedd879ad09572699165f05aba0
小俣圭佑 <omatakeisuke@MacBook.local>
2020-04-15 11:00:40 +0900

    [update] bash.sh

--- Ruby/rails/other/bash.sh
+++ Ruby/rails/other/bash.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
+# 実行
+# -vxで処理が出力される
+sh -vx ./bash.sh
+
 # 権限を変更する
 chmod 777 hoge.sh
 
-# 代入
-# ""は文字列
-a="hoge"
-b="fuga"
-
-# 条件式
-if [$a -eq $b]; then
-  echo $a
-else
-  echo $b
-fi
-
-# {}は変数を明示
-# $は変数の値を参照
 hoge="ls"
 # ダブルクォート
 # 変数展開
-echo "${hoge}"
+echo "${hoge}" => ls
 
 # シングルクォート
 # 文字列
-echo '${hoge}'
+echo '${hoge}' =>&{hoge}
 
 # バッククォート
 # コマンド実行
-`echo ${hoge}`
+`echo ${hoge}` => bash.sh hoge.sh hoge.sh
 $(echo ${hoge})
 
 # 標準入力
@@ -43,3 +33,24 @@ $(echo ${hoge})
 /dev/null
 # 出力を全て破棄する
 command > /dev/null 2>&1
+
+# 代入
+# ""は文字列
+a="hoge"
+b="fuga"
+
+# 条件式
+# スペースを入れないとエラー
+if [ "${a}" = "${b}" ]; then
+  echo "${a}"
+else
+  echo "${b}"
+fi
+
+hoge="fuga"
+# fugaをファイル名に含む全て
+ALL=`ls "${hoge}"*`
+for i in "${ALL}"
+do
+  echo "${i}"
+done
