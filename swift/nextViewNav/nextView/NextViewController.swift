21be06ad937aace85703c27e47f93ee9487d1631
小俣圭佑 <omatakeisuke@MacBook.local>
2020-05-06 15:37:48 +0900

    [add] nextViewNav.swift

---
+++ swift/nextViewNav/nextView/NextViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NextViewController.swift
+//  nextView
+//
+//  Created by 小俣圭佑 on 2020/05/06.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    @IBOutlet weak var label2: UILabel!
+    var count2 = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label2.text = String(count2)
+    }
+
+}
