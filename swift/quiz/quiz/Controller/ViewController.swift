26680d975a82007ad7d695f537421f8071f7f464
小俣圭佑 <omatakeisuke@MacBook.local>
2020-05-24 13:13:45 +0900

    [edit] quiz.swift

--- swift/quiz/quiz/Controller/ViewController.swift
+++ swift/quiz/quiz/Controller/ViewController.swift
@@ -12,20 +12,40 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var maxScoreLabel: UILabel!
+    let imagesList = ImagesList()
+    var pickedAnswer = false
+    var correctCount = 0
+    var wrongCount = 0
+    var maxScore = 0
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        correctCount = 0
+        wrongCount = 0
+        questionNumber = 0
+        
+        imageView.image = UIImage(named: imagesList.list[0].imageText)
     }
     
     @IBAction func answer(_ sender: Any) {
         //タグで分岐させる
         if (sender as AnyObject).tag == 1 {
-            
+            pickedAnswer = true
         } else if (sender as AnyObject).tag == 2 {
-            
+            pickedAnswer = false
         }
+        
+        check()
+    }
+    
+    func check(){
+        
     }
     
 }
