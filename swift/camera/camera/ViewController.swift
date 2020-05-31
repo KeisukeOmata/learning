f3f0956e7eb2618a89a425cd1dec3fc306c91e1a
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-01 08:15:03 +0900

    [add] camera.swift

---
+++ swift/camera/camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by 小俣圭佑 on 2020/06/01.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
