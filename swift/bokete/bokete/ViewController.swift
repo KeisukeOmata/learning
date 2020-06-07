78862dc1981362f554d0051b49fb3a0f643ffdec
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-08 08:37:45 +0900

    [edit] memo.swift

---
+++ swift/bokete/bokete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bokete
+//
+//  Created by 小俣圭佑 on 2020/06/08.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
