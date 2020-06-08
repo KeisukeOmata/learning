a3318557e0fd4a119217d13fea406134e4abecfe
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-09 08:47:16 +0900

    [edit] bokete.swift

--- swift/bokete/bokete/ViewController.swift
+++ swift/bokete/bokete/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import SDWebImage
+import Photos
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var odaiImageView: UIImageView!
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        commentTextView.layer.cornerRadius = 20.0
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            switch(status) {
+                case .authorized: 
+                    break
+                case .denied: 
+                    break
+                case .notDetermined:
+                    break
+                case .restricted:
+                    break
+            }
+        }
+        
+        func getImages(keyword: String) {
+            //16952655-0ebaa6fef1329e2a497eeda7e
+            let url = "https://pixabay.com/api/?key=16952655-0ebaa6fef1329e2a497eeda7e&q=\(keyword)"
+        }
+        
     }
-
 
 }
 
