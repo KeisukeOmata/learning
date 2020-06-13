1ab30bb49bf84bdccd8d82823bfaccc8330adb6c
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-13 18:54:18 +0900

    [add] FacebookLogin

---
+++ swift/FacebookLogin/FacebookLogin/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FacebookLogin
+//
+//  Created by 小俣圭佑 on 2020/06/13.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+//https://console.firebase.google.com/
+//プロジェクトを追加し、iOSを選択
+//プロジェクトのアプリ名(FacebookLogin)のBundle Identifierを入力
+//GoogleService-Info.plistをプロジェクトに追加
+//AuthenticationからFacebookを有効にする
+//https://developers.facebook.com/
+//アプリを作成
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
