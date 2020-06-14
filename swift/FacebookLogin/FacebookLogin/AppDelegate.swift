a4ff8f07f6172f723beea2a3f3c956fa8d878295
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-14 15:10:02 +0900

    [edit] FacebookLogin

--- swift/FacebookLogin/FacebookLogin/AppDelegate.swift
+++ swift/FacebookLogin/FacebookLogin/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,11 +16,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        //Facebookログイン
+        FirebaseApp.configue()
+        application.shared.application(application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
+    
+    //Facebookログイン
+    func application(_ application: UIApplication,open url: URL,sourceApplication: String?,annotation: Any) -> Bool {
+        return ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    //Facebookログイン
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppEvents.activateApp()
+    }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
