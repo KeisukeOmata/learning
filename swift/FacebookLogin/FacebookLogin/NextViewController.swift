888e0c6dc9d50b31b7cd7544db52c801c8f7ace5
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-17 22:35:59 +0900

    [edit] FacebookLogin

--- swift/FacebookLogin/FacebookLogin/NextViewController.swift
+++ swift/FacebookLogin/FacebookLogin/NextViewController.swift
@@ -7,24 +7,16 @@
 //
 
 import UIKit
+import SDWebImage
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
