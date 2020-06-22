d8142be9eeea22cfb3d9dc05bd525cae3efa7543
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-23 06:06:47 +0900

    [edit] Ad.swift

--- swift/Ad/Ad/ViewController.swift
+++ swift/Ad/Ad/ViewController.swift
@@ -6,13 +6,27 @@
 //  Copyright © 2020 KeisukeOmata. All rights reserved.
 //
 
+//GoogleAdMob
+//アプリを追加
+//広告ユニットを追加
+//info.plistのソースコードにGADApplicationIdentifierキーとAdMobアプリIDの文字列を追加
+
 import UIKit
 import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    //クラスはGADBannerView
+    @IBOutlet weak var BannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //バナー広告の読み込み
+        //テストID
+        BannerView.adUnitID = "ca-app-pub-3940256099942544~1458002511"
+        BannerView.rootViewController = self
+        BannerView.load(GADRequest())
     }
 
 
