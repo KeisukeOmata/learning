d0857f6b581db9a20b11a4b49cc9e756c8828e4e
小俣圭佑 <omatakeisuke@MacBook.local>
2020-06-26 20:00:43 +0900

    [add] ChatApp

---
+++ swift/ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 小俣圭佑 on 2020/06/27.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
