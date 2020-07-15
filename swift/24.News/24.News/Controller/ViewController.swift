ccde58df6fcd308ad9280f1221e7125ba2cd2d3b
小俣圭佑 <omatakeisuke@MacBook.local>
2020-07-16 08:52:29 +0900

    [edit] News.swift

--- swift/24.News/24.News/Controller/ViewController.swift
+++ swift/24.News/24.News/Controller/ViewController.swift
@@ -7,13 +7,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollScrollVIew: UIScrollView!
     var jsonArray = ["1", "2", "3", "4", "5"]
-    var jsonTextArray = ["text1", "text1", "text1", "text1", "text1"]
+    var jsonTextArray = ["text1", "text2", "text3", "text4", "text5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //スクロールビューを有効にする
         scrollScrollVIew.isPagingEnabled = true
+        //スクロールビューのサイズとテキストの設定を行う
+        func scroll()
     }
     
     //ナビゲーションバーを非表示にする
@@ -22,14 +24,38 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         self.navigationController?.isNavigationBarHidden = true
     }
     
+    //スクロールビューのサイズとテキストの設定を行う
     func scroll() {
         scrollScrollVIew.delegate = self
-        //スクロールビューのサイズ
+        
+        //スクロールビューのサイズを決める
         //5枚表示するのでwidthに＊5
-        scrollScrollVIew.contentSize = CGSize(width: view.frame.size.width * 5, height: scrollScrollVIew.frame.size.height)
-        //ラベルをつける
+        scrollScrollVIew.contentSize = CGSize(
+                                            width: view.frame.size.width * 5,
+                                            height: scrollScrollVIew.frame.size.height
+                                        )
+        
+        //スクロールビューにテキストをつける
         for i in 0...4 {
+            let label = UILabel(
+                            frame: CGRect(
+                                //xとyは画面サイズ
+                                x: CGFloat(i) * view.frame.size.width,
+                                y: view.frame.size.height / 3,
+                                //widthとheightはスクロールビューのサイズ
+                                width: scrollScrollVIew.frame.size.width,
+                                height: scrollScrollVIew.frame.size.height
+                            )
+                        )
             
+            //フォントサイズ
+            label.font = UIFont.boldSystemFont(ofSize: 15.0)
+            //表示位置
+            label.textAlignment = .center
+            //スクロールビューに表示するテキスト
+            label.text = jsonTextArray[i]
+            //スクロールビューに設定する
+            scrollScrollVIew.addSubview(label)
         }
     }
 
