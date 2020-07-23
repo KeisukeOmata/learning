e41d2aae902dca30a5eff20dec7512c2a3c13812
小俣圭佑 <omatakeisuke@MacBook.local>
2020-07-23 21:23:48 +0900

    [edit] memo.swift

---
+++ swift/24.News/24.News/Controller/WebViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WebViewController.swift
+//  24.News
+//
+//  Created by 小俣圭佑 on 2020/07/23.
+//  Copyright © 2020 KeisukeOmata. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
