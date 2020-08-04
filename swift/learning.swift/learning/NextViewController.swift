9d870e052eecebebb6e9527ef57fe55b6175c0a5
小俣圭佑 <omatakeisuke@MacBook.local>
2020-08-04 21:45:24 +0900

    [edit] Marge NextView to learning.swift

---
+++ swift/learning.swift/learning/NextViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    var fuga: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(fuga)
+    }
+
+}
