157a76a5ee88d62d71a816d01f81c43f7cccf109
小俣圭佑 <omatakeisuke@MacBook.local>
2020-08-09 18:32:00 +0900

    [edit] memo.swift

--- swift/memo.swift
+++ swift/memo.swift
@@ -15,7 +15,7 @@ pod install
 //変数宣言時以外の?, !はオプショナル型とは無関係
 //何に使うの => 設定しなくて良い画像とか
 var hoge: Int?
-println(hoge) => nil
+print(hoge) => nil
 
 var a: Int  = 10
 var b: Int? = 10
@@ -32,18 +32,18 @@ a + b
 => 20
 ---------------------------------------------
 var piyo: String? = "hello world"
-println(piyo) => Optional("hello world")
+print(piyo) => Optional("hello world")
 
 //強制的アンラップ
 //nilだと落ちる
-println(piyo!) => "hello world"
+print(piyo!) => "hello world"
 ---------------------------------------------
 //オプショナルバインディング
 if let unwrappedPiyo = piyo {
-    println(unwrappedPiyo)
+    print(unwrappedPiyo)
 }
 else {
-    println("piyoはnilです")
+    print("piyoはnilです")
 }
 //条件式で代入された変数は、アンラップされたものとなる
 => "hello world"
@@ -56,6 +56,7 @@ dog?.hello()
 => "Optional(ワン)"
 
 //nilに対してプロパティを取得しようとしたり、メソッドを呼び出すことがなくなる
+//初期化をしていないとき
 var cat: cat?
 cat?.name
 => nil
