62aa648b299fb53021561ca0b7ac547094ce4868
小俣圭佑 <omatakeisuke@MacBook.local>
2020-08-16 15:07:04 +0900

    [add] VSCode.rb

---
+++ Ruby/rails/init/VSCode.rb
@@ -0,0 +1,10 @@
+# 全角半角を可視化
+settings.jsonを編集
+"editor.renderWhitespace": "all",
+zenkaku
+.vscode/extensions/mosapride.zenkaku-◯.◯.◯/extension.js
+5行目のvar enabled = false; を var enabled = true; に変更
+
+# インデントに色を付ける
+indent-rainbow
+設定からcolorで検索し、setting.jsonで編集を選択
