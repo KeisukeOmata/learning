8e5c9b32b34e51101e0dc871b7be9131d75c3b26
小俣圭佑 <omatakeisuke@MacBook.local>
2020-09-23 12:50:32 +0900

    [add] Active Storageのバリデーションを追加

--- rails/learning_rails/app/models/user.rb
+++ rails/learning_rails/app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   has_many :comments
   # Action Text
   has_rich_text :name
+
+  # direct_upload: trueの場合、pngなどの拡張子に偽装したテキストファイルがエラーにならないので注意
+  validates :portrait,
+    # Active Storageの画像の拡張子に制限をかける
+    content_type: [:png, :jpg, :jpeg],
+    # Active Storageの画像の容量に制限をかける
+    size: { less_than_or_equal_to: 10.megabytes },
+    # Active Storageの画像のwidth, heightに制限をかける
+    dimention: { width: { max: 2000 }, height: { max: 2000 } }
 end
