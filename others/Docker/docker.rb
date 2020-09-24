90ed2903926cab441d66d27f96e5fca2d55599b7
小俣圭佑 <omatakeisuke@MacBook.local>
2020-09-24 16:15:46 +0900

    [add] docker.rbに追記

--- others/Docker/docker.rb
+++ others/Docker/docker.rb
@@ -2,16 +2,25 @@
 docker attach
 # コンテナから抜ける(デタッチ)
 Ctrl + Q + P
-# コンテナ起動
+# コンテナ開始
 docker start
+# コンポーズコンテナ開始
+docker-compose start
 # コンテナ停止
 docker stop
+# コンポーズコンテナ停止
+docker-compose stop
+# コンポーズコンテナの再起動
+docker-compose restart
 ----------------------------
 # コンテナ一覧
 docker ps -a
+# コンポーズコンテナ一覧
 docker-compose ps
 # コンテナ削除
 docker rm -f コンテナID
+# コンポーズコンテナ削除
+docker-compose rm -f
 # イメージ一覧
 docker images
 # イメージ削除
@@ -21,54 +30,55 @@ docker image prune
 # イメージの取得
 docker pull
 ----------------------------
-# コンテナの再起動
-docker-compose restart
-# コンテナの開始
-docker-compose start
-# コンテナの停止
-docker-compose stop
-----------------------------
-# docker-composeの作成
-# イメージも作成される
-# 複数のコンテナイメージを組み合わせる
-# up => コンテナの作成と開始
+# docker-compose
+# 1.mysqlコンテナを作成し、バックグラウンドで起動
 docker-compose up -d mysql
-# mysql起動
+# 2.execコマンドで既に起動中のmysqlコンテナに対して、コマンド(mysql -u root -p)を実行する
 docker-compose exec mysql mysql -u root -p
-CREATE database hoge CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
-CREATE database hoge_test CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
-GRANT ALL on appdb.* TO hoge@'%' IDENTIFIED BY 'password';
-GRANT ALL on appdb_test.* TO hoge@'%';
-# gem 'mysql2', '>=0.4.4'
-# database.ymlを更新
-# run => コンテナの作成と開始、引数で指定したサービスのコンテナ内でコマンドを実行
+-------------------
+CREATE database appdb CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
+CREATE database appdb_test CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;
+GRANT ALL on appdb.* TO app@'%' IDENTIFIED BY 'password';
+GRANT ALL on appdb_test.* TO app@'%';
+-------------------
+# 3.Gemを追加
+gem 'mysql2', '>=0.4.4'
+# 4.database.ymlを更新
+# 5.appイメージとコンテナの作成
 docker-compose run --service-ports app
+# 6.各種ダウンロード
 bundle install
 yarn install
 apt update; apt -y upgrade
-apt install vim apt-utils
+# vimのインストール
+apt-get update;apt-get install vim
+# 7.起動
 bin/rails db:migrate
 bin/rails s -b 0.0.0.0
 ----------------------------
-# # コンテナの作成とログイン
-# docker run -it コンテナ名
-# # ポートを指定
-# docker run -p 3000:3000 コンテナ名
-# ----------------------------
 # # イメージ作成
 # docker build -t イメージ名 .
 # # マウントキャッシュを使う場合
 # DOCKER_BUILDKIT=1 docker build -t イメージ名 -f Dockerfile-buildkit .
 ----------------------------
-# 開発環境用の場合
-docker volume create fav_map_rails_bundle
-docker volume create fav_map_rails_node_modules
-docker run -i -t \
--v $(pwd):/app:cached \
--v learning_rails_bundle:/app/vendor/bundle \
--v learning_rails_node_modules:/app/node_modules \
--p 3000:3000 \
-learning_rails
+# # コンテナの作成とログイン
+# docker run -it コンテナ名
+# # ポートを指定
+# docker run -p 3000:3000 コンテナ名
+----------------------------
+# # rails単体で動かす
+# docker build -t fav_map .
+# docker volume create fav_map_bundle
+# docker volume create fav_map_node_modules
+# docker run -i -t \
+# -v $(pwd):/app:cached \
+# -v fav_map_bundle:/app/vendor/bundle \
+# -v fav_map_node_modules:/app/node_modules \
+# -p 3000:3000 \
+# fav_map
+# bundle install
+# yarn install
+# bin/rails s -b 0.0.0.0
 ----------------------------
 # コンテナを使う場合はログを標準出力に変更する(config/environments/production.rb)
 config.logger = ActiveSupport::Logger.new($stdout)
@@ -77,3 +87,32 @@ $stdout.sync = true
 # redis
 docker pull redis
 docker run -p 6379:6379 redis
+----------------------------
+# Dockerのインストール
+# 古いDockerを削除
+1. sudo apt-get remove docker docker-engine docker.io containerd runc
+2. sudo apt-get update
+# 前提ソフトウェアのインストール
+3. sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+# GPG公開鍵をインストール
+4. curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# 公開鍵のフィンガープリントを確認
+5. sudo apt-key fingerprint 0EBFCD88
+# aptリポジトリの設定
+6. sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+7. sudo apt-get update
+# Dockerのインストール
+8. sudo apt-get install -y docker-ce
+9. docker -v
+# Docker Composeのインストール
+10. sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+11. sudo chmod +x /usr/local/bin/docker-compose
+12. docker-compose --version
