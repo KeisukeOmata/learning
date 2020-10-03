03de641f9949be23e97431dcbefc94458e8fd113
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-03 19:50:54 +0900

    [add] learning.reactに条件分岐を追加

--- react/learning.react/src/index.js
+++ react/learning.react/src/index.js
@@ -8,8 +8,38 @@ import * as serviceWorker from './serviceWorker';
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+let title = "hoge"
+let url = "https://github.com/KeisukeOmata/learning"
+let flg = true;
+let print = function(msg, size, color){
+  const style = {
+    fontSize: size + "pt",
+    fontWeight:'700',
+    color: color,
+    border: "1px solid " + color
+  };
+  return <p style={style}>{msg}</p>;
+}
 let dom = document.querySelector('#root');
-let element = React.createElement(
-  'p', {}, 'Hello React Application!'
+let el = (
+  // renderできるのは1つのエレメントだけ
+  // 変数展開は{}
+  // 条件分岐は{真偽値 ? trueのJSX : falseのJSX}
+  <div>
+    {flg ?
+      <div>
+        <h2>{title}</h2>
+        <p><a href={url}>github</a></p>
+        {print('first', 30, 'red')}
+        {print('second', 20, 'blue')}
+        {print('third', 10, 'green')}
+      </div>
+    :
+      <div>
+        <h2>真偽値がfalse</h2>
+      </div>
+    }
+  </div>  
 );
-ReactDOM.render(element, dom);
+  
+ReactDOM.render(el, dom);
