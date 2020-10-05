b040259e9cbe8c5c6fd3232cf46695438daa118b
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-06 08:51:32 +0900

    [add] 複数のコンポーネントによる値の受け渡しを追加

--- react/learning.react/src/index.js
+++ react/learning.react/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Square from './square';
 import State from './state';
+import ListComponent from './listComponent';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Square x="100" y="100" w="100" h="100" c="cyan" />, document.getElementById('square1'));
 ReactDOM.render(<Square x="150" y="150" w="100" h="100" c="magenta" />, document.getElementById('square2'));
 ReactDOM.render(<State />, document.getElementById('state'));
+ReactDOM.render(<ListComponent />, document.getElementById('listComponent'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
