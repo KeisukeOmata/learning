8f67ea9c8488043232e72a0d6bd73cd8ef676cf9
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-07 07:48:21 +0900

    [add] 子要素の扱いを追加

---
+++ react/learning.react/src/childComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// childComponentコンポーネント
+class ChildComponent extends Component {
+  // コンストラクタ
+  constructor(props) {
+    super(props);
+  };
+
+  // レンダー
+  render() {
+    return <div>
+      {/* Messageコンポーネントにタイトルを渡す */}
+      <Message title="Children!">
+        文章1。文章2。文章3。
+      </Message>
+    </div>;
+  };
+};
+
+// Messageコンポーネント
+class Message extends Component {
+  listStyle = {
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "0px"
+  };
+
+  // レンダー
+  render() {
+    // this.props.childrenで子要素を代入
+    let contents = this.props.children;
+    // .splitで任意の文字で区切った配列にする
+    let sentenceArray = contents.split('。');
+    let sentenceArray2 = [];
+
+    // .lengthで配列の要素数を取得
+    for (let i = 0; i < sentenceArray.length; i++) {
+      // .trimで文字列の両橋の空白を削除
+      // 配列の要素が''でなければsentenceArray2に追加
+      if(sentenceArray[i].trim() != '') {
+        sentenceArray2.push(sentenceArray[i]);
+      };
+    };
+
+    // .mapで配列を展開
+    let lists = sentenceArray2.map((value,key) => (
+      <li style={this.listStyle} key={key}>{value}.</li>)
+    );
+
+    // リターン
+    return <div>
+      <h2>{this.props.title}</h2>
+      <ol>{lists}</ol>
+    </div>;
+  };
+};
+
+export default ChildComponent;
--- react/learning.react/src/index.js
+++ react/learning.react/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import Square from './square';
 import State from './state';
 import ListComponent from './listComponent';
+import ChildComponent from './childComponent';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -12,6 +13,7 @@ ReactDOM.render(<Square x="100" y="100" w="100" h="100" c="cyan" />, document.ge
 ReactDOM.render(<Square x="150" y="150" w="100" h="100" c="magenta" />, document.getElementById('square2'));
 ReactDOM.render(<State />, document.getElementById('state'));
 ReactDOM.render(<ListComponent />, document.getElementById('listComponent'));
+ReactDOM.render(<ChildComponent />, document.getElementById('childComponent'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
