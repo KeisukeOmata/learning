1f55c7db87d34b6eceb15e58be1459a89ab2ce1d
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-11 07:13:05 +0900

    [add] next.jsに画像を追加

--- react/learning.next/pages/index.js
+++ react/learning.next/pages/index.js
@@ -1,10 +1,22 @@
 import Link from 'next/link';
 import Layout from '../components/layout';
+import Image from '../static/image';
 
 export default () => (
-  <Layout header="次へ" title="トップページ">
-    <p>hello world</p>
-    <Link href="./other">
+  <Layout
+    header="次へ"
+    title="トップページ"
+  >
+    <p>
+      hello world
+    </p>
+    <Image
+      fileName="image.jpg"
+      size="250"
+    />
+    <Link
+      href="./other"
+    >
       <button>
         next &gt;&gt;
       </button>
