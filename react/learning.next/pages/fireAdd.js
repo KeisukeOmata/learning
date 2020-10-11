a476451c8bea91772a256fdd45fb4b304207b1fc
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-12 07:06:30 +0900

    [add] next.jsにfirebaseへのデータ追加機能を追加

---
+++ react/learning.next/pages/fireAdd.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import FireAdd from '../components/fireAdd';
+
+const fireAdd = () =>(
+  <Layout header="Fire" title="add data.">
+    <FireAdd />
+  </Layout>
+ );
+
+export default fireAdd
