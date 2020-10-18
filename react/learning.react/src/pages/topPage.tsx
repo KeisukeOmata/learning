4ccdc746b7fd2f9d4731441e62e1ee85682a1dd3
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-18 16:55:50 +0900

    [add] material-uiのIconButton, Paper, InputBase, iconを追加

--- react/learning.react/src/pages/topPage.tsx
+++ react/learning.react/src/pages/topPage.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
 import TopHeader from "../components/topPage/topHeader";
+import TopMain from "../components/topPage/topMain";
 
 const TopPage: FC = () => {
   return (
-    <TopHeader />
+    <>
+      <TopHeader />
+      <TopMain />
+    </>
   );
 }
 
