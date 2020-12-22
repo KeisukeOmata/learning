9a2265bf7eacff8df37bbfec9b7bfa3ebd3ee788
小俣圭佑 <omatakeisuke@MacBook.local>
2020-12-23 07:50:20 +0900

    Rename

--- react_memo.rb
+++ react_memo.rb
@@ -10,15 +10,89 @@ mv pages/index.js pages/index.tsx
 mv pages/_app.js pages/_app.tsx
 yarn add --dev typescript @types/react @types/node
 # src/components, foundations, layouts, pages, typesを作成
+# tailwind
+yarn add -D tailwindcss
+yarn tailwindcss init 
+yarn add -D postcss-preset-env
+yarn add -D postcss  
+yarn add -D @tailwindcss/ui
+PostCSS Language Support # 拡張
+# prettier
+yarn add -D prettier
+touch .prettierrc.js
+# eslint
+yarn add -D eslint eslint-plugin-react \
+              eslint-config-prettier eslint-plugin-prettier \
+        @typescript-eslint/parser @typescript-eslint/eslint-plugin
+touch .eslintrc.js
+# huskey
+yarn add lint-staged
+yarn add husky
+# jest
+yarn add -D jest identity-obj-proxy ts-jest @types/jest
+touch jest.config.js
+# react testing-library
+yarn add -D @testing-library/react
+# cypress
+yarn add -D cypress
+yarn run cypress open
+find cypress -name "*.js" | sed 'p;s/.js$/.ts/' | xargs -n2 mv
+touch cypress.json
+touch cypress/integration/index_spec.ts
+yarn cypress:open
+# pwa
+yarn add next-pwa
+touch next.config.js
+yarn add pwacompat
+# storybook
+yarn add -D @storybook/react @babel/core babel-loader babel-preset-react-app
+# package.json
+"storybook": "start-storybook -p 6006 -c .storybook"
+mkdir .storybook
+cd .storybook
+main.js, preview.tsをコピーしてくる
+cd src 
+mkdir stories
+cd stories
+touch Button.stories.tsx
+# src/stories/Button.stories.tsx
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button from '../components/Button';
+
+storiesOf('Button', module).add('with text', () => {
+  return <Button text="Click Me" />;
+});
+yarn storybook
+# storybookできないVer
+npx sb init
+yarn add -D @storybook/addon-storyshots
+mkdir storyshots && \
+  touch storyshots/storybook.test.ts
+mv .storybook/preview.js .storybook/preview.ts
+yarn add -D jest-svg-transformer
+yarn add -D @storybook/addon-storyshots-puppeteer puppeteer
+touch storyshots/puppeteer-storyshots.test.ts
+touch storyshots/puppeteer-storyshots-ipad.test.ts
+touch storyshots/puppeteer-storyshots-iphone.test.ts
+npm i --save-dev @types/puppeteer
+
+yarn add --dev @storybook/react ts-loader css-loader babel-preset-react-app babel-plugin-react-require 
+
+yarn run storybook
+yarn storyshots
+yarn puppeteer-storyshots
 # 各種インストール
+yarn add @types/swiper
 yarn add vercel
 yarn add @types/axios
 yarn add @types/firebase
 yarn add firebase-admin
-yarn add @types/recoil
+yarn add types/recoil
 yarn add bootstrap@next
 yarn add sass
 yarn add @material-ui/core
+yarn add @material-ui/lab
 # アニメーション
 yarn add @types/react-toastify
 # 日時を扱う
@@ -27,6 +101,19 @@ yarn add dayjs
 yarn add canvas
 # リセットcss
 yarn add minireset.css
+
+yarn lint
+yarn lint --fix
+
+# Shopify
+brew tap shopify/shopify
+brew install shopify-cli
+shopify version
+# gem list shopify_api
+# gem install shopify_api
+# gem uninstall shopify_api -v x.y.z
+shopify create
+shopify serve
 ーーーーーーーーーーーーーーーーーーーーーーーー
 # jsonを環境変数に設定
 # 環境変数にbase64でエンコードしたjsonファイルの内容を指定し、それを実行時にデコードして利用
