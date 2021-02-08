456d0444722955ba1b0d37fe2967c8c887d087cd
小俣圭佑 <omatakeisuke@MacBook.local>
2021-02-09 07:22:40 +0900

    Remove files that are not in use

--- rails_map_app/db/migrate/20190813033520_add_category_to_livehouse.rb
+++
@@ -1,7 +0,0 @@
-class AddCategoryToLivehouse < ActiveRecord::Migration[5.2]
-  def change
-    add_column :livehouses, :category, :integer
-    add_column :livehouses, :livehouse_image_id, :text
-    add_column :livehouses, :livehouse_body, :text
-  end
-end
